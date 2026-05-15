6bc6adbf5b29ebf2e3d8247957290a5321577581
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2026-05-15 23:26:13 +0530

    minor change

--- server/config/db.js
+++ server/config/db.js
@@ -3,13 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const connectionString =
+  process.env.MYSQL_PUBLIC_URL;
+
+const db = await mysql.createConnection(
+  connectionString
+);
 
 console.log("MySQL Connected");
 
