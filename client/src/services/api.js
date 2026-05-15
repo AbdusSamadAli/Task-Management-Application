8bba51784002c0138052a7b92bedf11862831f38
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2026-05-15 23:48:33 +0530

    minor change

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,27 +1,21 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL,
+  baseURL: "/api",
 });
 
-API.interceptors.request.use(
-  (req) => {
-    const userInfo =
-      localStorage.getItem(
-        "userInfo"
-      );
+API.interceptors.request.use((req) => {
+  const userInfo =
+    localStorage.getItem("userInfo");
 
-    if (userInfo) {
-      const token = JSON.parse(
-        userInfo
-      ).token;
+  if (userInfo) {
+    const token = JSON.parse(userInfo).token;
 
-      req.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return req;
+    req.headers.Authorization =
+      `Bearer ${token}`;
   }
-);
+
+  return req;
+});
 
 export default API;
